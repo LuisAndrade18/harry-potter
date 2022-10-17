c38b38969479e2e22f383bf35741f5415ce5d2be
LuisAndrade18 <felipeluis2002@gmail.com>
2022-10-17 11:54:39 -0300

    teste 3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import React from "react";
-import Header from "./Components/Header";
-import Main from "./Components/Main";
+import React from 'react';
+import Header from './Components/Header';
+import Main from './Components/Main';
 
-
-const App = () =>{
-  return(
+const App = () => {
+  return (
     <div>
-<Header/>
-<Main/>
+      <Header />
+      <Main />
     </div>
- 
-  )
-}
-export default App
+  );
+};
+export default App;
