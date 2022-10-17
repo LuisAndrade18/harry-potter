63fa11483090bc42814ec7c26e152fc53d3e2c87
LuisAndrade18 <felipeluis2002@gmail.com>
2022-10-17 11:36:49 -0300

    teste deploy

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
-import * as S from "../Styles/styled-main";
+import * as S from "../Styles/Styled-main";
 
 const Main = () => {
   const [card, setCard] = useState([]);
